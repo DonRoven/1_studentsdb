2a9c0d1cfbe9d142644ae3401747bf2adf7c665e
Vitaliy <si_vitalik@yahoo.com>
2017-03-03 18:29:06 +0200

    demo storinka

--- studentsdb/studentsdb/urls.py
+++ studentsdb/studentsdb/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from students import views
 
 urlpatterns = [
+    url(r'^$', views.students_list, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
