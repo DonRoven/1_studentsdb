5377e177060b4d729495f65ad14bbfa152514661
Vitaliy <si_vitalik@yahoo.com>
2017-03-06 20:59:00 +0200

    optimaise view1

---
+++ studentsdb/students/views/students.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+#Views for Students
+def students_list(request):
+    students = (
+        {'id': 1,
+         'first_name': u'Віталій',
+         'last_name': u'Подоба',
+         'ticket': 235,
+         'image': 'img/me.jpeg'},
+        {'id': 2,
+         'first_name': u'Андрій',
+         'last_name': u'Корост',
+         'ticket': 2235,
+         'image': 'img/piv.png'},
+        {'id': 3,
+         'first_name': u'Андрій',
+         'last_name': u'Сеньківський',
+         'ticket': 235,
+         'image': "img/podoba3.jpg"}
+    )
+    return render(request, 'students/students_list.html', {'students': students})
+
+def students_add(request):
+    return HttpResponse('<h1>Students Add Form</h1>')
+
+def students_edit(request, sid):
+    return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+
+def students_delete(request, sid):
+    return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+
