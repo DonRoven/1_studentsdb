301543a3e8ddb1fd51a4b4b0d1b337427cf5a26c
Vitaliy <si_vitalik@yahoo.com>
2017-03-06 20:10:39 +0200

    context_processors

--- studentsdb/studentsdb/settings.py
+++ studentsdb/studentsdb/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'studentsdb.context_processors.students_proc',
             ],
         },
     },
@@ -117,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PORTAL_URL = 'http://localhost:8000'
